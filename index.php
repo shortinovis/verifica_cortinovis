7a16bb65ca95a4037f15d5a6dd4f82322bfeaab9
shortinovis <cortinovis.matteo.studente88@itispaleocapa.it>
2026-05-05 11:23:57 +0200

    modifiche varie

--- index.php
+++ index.php
@@ -1,22 +1,78 @@
+<?php require_once 'controller.php'; ?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>palestra cortinovis</title>
 </head>
 <body>
-    <div>
-        <p>aggiunta iscritto a un corso</p>
-        <a href="inserimento.php"></a>
-    </div>
 
-    <div>
-        <p>visualizzazione corsi</p>
-    </div>
-        <p>elenco iscritti a un corso</p>
-    <div>
+    <h1>palestra cortinovis</h1>
 
-    </div>
+    <h2>Inserisci iscritto</h2>
+    <form method="POST">
+        id_membro: <input type="text" name="id" required><br><br>
+        nome: <input type="text" name="nome" required><br><br>
+        cognome: <input type="text" name="cognome" required><br><br>
+        data_nascita: <input type="date" name="nascita" required><br><br>
+        tipo_abbonamento: <input type="text" name="abbonamento" required><br><br>
+        stato_pagamento: <input type="number" name="status" required><br><br>
+
+        Istruttore:
+        <select name="istruttore">
+            <?php foreach($istruttori as $a) { ?>
+                <option value="<?php echo $a['i']; ?>">
+                    <?php echo $a['nome'] . " " . $a['cognome']; ?>
+                </option>
+            <?php } ?>
+        </select><br><br>
+
+        <button type="submit" name="nuovo_iscritto">Aggiungi</button>
+    </form>
+
+    <hr>
+
+    
+    <h2>Visualizza corsi</h2>
+    <form method="GET">
+        <select name="istruttore">
+            <?php foreach($istruttori as $u) { ?>
+                if($u)
+                <option value="<?php echo $u['id_utente']; ?>">
+                    <?php echo $u['nome'] . " " . $u['cognome']; ?>
+                </option>
+            <?php } ?>
+        </select>
+
+        <button type="submit">Visualizza</button>
+    </form>
+
+    <br><br>
+
+    <?php if($prestiti != null) { ?>
+        <table border="1">
+            <tr>
+                <th>Libro</th>
+                <th>Stato</th>
+                <th>Azione</th>
+            </tr>
+
+            <?php foreach($prestiti as $p) { ?>
+                <tr>
+                    <td><?php echo $p['titolo']; ?></td>
+                    <td><?php echo $p['restituito'] ? "Restituito" : "In prestito"; ?></td>
+                    <td>
+                        <?php if(!$p['restituito']) { ?>
+                            <a href="?utente_id=<?php echo $_GET['utente_id']; ?>&restituisci=<?php echo $p['id_prestito']; ?>">
+                                Restituisci
+                            </a>
+                        <?php } ?>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+    <?php } ?>
+
 </body>
 </html>
